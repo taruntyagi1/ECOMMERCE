43e72c34269fa086afd3da5fd8eed9c2fddfc7c6
Tarun Tyagi <tarun.tyagi.cs.2018@mitmeerut.ac.in>
2023-03-06 19:09:37 +0530

    aws database configuration

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -92,8 +92,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Ecommerce',
         'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
+        'PASSWORD': 'tarunroot',
+        'HOST': 'database-2.cvuk9hr7ijcp.us-east-2.rds.amazonaws.com',
         'PORT': '5432',
        
     }
