b05fa091580bf8fbee10812270a7eb72a0e38dfe
Tarun Tyagi <tarun.tyagi.cs.2018@mitmeerut.ac.in>
2023-03-06 18:49:37 +0530

    user edit address

--- products/urls.py
+++ products/urls.py
@@ -13,11 +13,18 @@ urlpatterns = [
     path('login/',LoginView.as_view(),name='login'),
     path('logout/',Logout.as_view(),name='logout'),
     path('contact/',ContactView.as_view(),name = 'contact'),
-    path('user_dashboard/',UserDashboard.as_view(),name='user_dashboard'),
+    path('user_account/',UserAccount.as_view(),name='user_account'),
     path('register/',UserRegister.as_view(),name='register'),
     path('activate/<token>/<uidb64>/',views.activate_account,name='activate'),
     path('basket/',Basket.as_view(),name='basket'),
     path('filter_name/',FilterName.as_view(),name='filter_name'),
-    path('user_address_form/',User_address_edit.as_view(),name='user_address_form')
+    path('user_address_edit/', UserAddressEdit.as_view(), name='user_address_edit'),
+    path('request_path_login/',Login_for_requested_path.as_view(),name = 'request_path_login'),
+
+    path('user_orders/',USerOrders.as_view(),name='user_orders'),
+    path('user_download/',User_download.as_view(),name='user_download'),
+    path('user_address_form/<int:address_id>/',UserAddressForm.as_view(),name='user_address_form'),
+    # path('user_address_form/<int:address_id>/',views.address_form,name='user_address_form')
+   
     
 ]
