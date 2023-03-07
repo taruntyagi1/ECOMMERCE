1f206b6181c6b39c52c01b374e95e673d58a8bcc
Ubuntu <ubuntu@ip-172-31-38-152.us-east-2.compute.internal>
2023-03-07 11:07:35 +0000

    staticfiles

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u62f#h5mn^eobb*%4(x0f*rh5ugf26vdy*g^@w)#9x_prfycjl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','18.191.197.255']
 
 
 # Application definition
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR , 'static/'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
