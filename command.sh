8b471d2bf30d4ddc2f50046eb262c55ea9c5bee7
Tarun Tyagi <tarun.tyagi.cs.2018@mitmeerut.ac.in>
2023-03-09 13:44:15 +0530

    venv fix

--- command.sh
+++ command.sh
@@ -74,3 +74,17 @@ ExecStart=/home/ubuntu/Myprojects/ECOMMERCE/venv/bin/gunicorn \
 
 [Install]
 WantedBy=multi-user.target
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Ecommerce',
+        'USER': 'postgres',
+        'PASSWORD': 'tarunroot',
+        'HOST': 'database-2.cvuk9hr7ijcp.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
+       
+    }
+}
