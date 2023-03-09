b8d2e7e0c00421184cfaec97e63ff8fda95464cd
Ubuntu <ubuntu@ip-172-31-38-152.us-east-2.compute.internal>
2023-03-09 06:14:58 +0000

    venv fix

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u62f#h5mn^eobb*%4(x0f*rh5ugf26vdy*g^@w)#9x_prfycjl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 
-ALLOWED_HOSTS = ['*','18.191.197.255']
+ALLOWED_HOSTS = ['*','18.191.197.255','www.tarun.com']
 
 
 # Application definition
