7b2920b829abe7a21aeadbc79e2a43836aaf55dc
Tarun Tyagi <tarun.tyagi.cs.2018@mitmeerut.ac.in>
2023-03-16 13:22:22 +0530

    run.sh

--- run.sh
+++ run.sh
@@ -9,3 +9,10 @@ source /home/ubuntu/Myprojects/ECOMMERCE/venv/bin/activate
 
 
 # Start Gunicorn
+exec gunicorn ecommerce.wsgi:application \
+    --name ecommerce \
+    --workers 3 \
+    --bind unix:/run/gunicorn.sock \
+    --access-logfile /var/log/gunicorn/access.log \
+    --error-logfile /var/log/gunicorn/error.log \
+    --log-level debug
