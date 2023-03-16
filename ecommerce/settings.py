45d3a15b96f046dd5d85124bccbb131a0185a395
Tarun Tyagi <tarun.tyagi.cs.2018@mitmeerut.ac.in>
2023-03-16 15:39:27 +0530

    settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -56,6 +56,17 @@ INSTALLED_APPS = [
     'cart',
     'orders'
 ]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Ecommerce',
+        'USER': 'postgres',
+        'PASSWORD': 'tarunroot',
+        'HOST': 'database-2.cvuk9hr7ijcp.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
+       
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
