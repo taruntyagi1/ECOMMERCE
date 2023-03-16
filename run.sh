49d63aea3b72d83cf36c0e56fbe0bd698a238f93
Tarun Tyagi <tarun.tyagi.cs.2018@mitmeerut.ac.in>
2023-03-16 15:25:48 +0530

    run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
 
 export ENV="production"  # or "local"
 
-exec gunicorn ecommerce.wsgi:application \
-  --bind unix:/run/gunicorn.sock \
-  --workers 3 \
-  --timeout 120 \
-  --access-logfile /var/log/gunicorn/access.log \
-  --error-logfile /var/log/gunicorn/error.log
