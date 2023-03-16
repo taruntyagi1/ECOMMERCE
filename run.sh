2113b3296f2999dbfd1a05d3243b541565adb8e4
Tarun Tyagi <tarun.tyagi.cs.2018@mitmeerut.ac.in>
2023-03-16 12:23:27 +0530

    run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
 
 
 export DJANGO_ENV=production
+
+# Activate virtual environment
+source /home/ubuntu/Myprojects/ECOMMERCE/venv/bin/activate
+
+# Collect static files
+
+
 # Start Gunicorn
-
-
-
-exec gunicorn -c /home/ubuntu/Myprojects/ECOMMERCE/gunicorn_conf.py ecommerce.wsgi
+exec gunicorn ecommerce.wsgi:application \
+    --name ecommerce \
+    --workers 3 \
+    --bind unix:/run/gunicorn.sock \
+    --access-logfile /var/log/gunicorn/access.log \
+    --error-logfile /var/log/gunicorn/error.log \
+    --log-level debug
