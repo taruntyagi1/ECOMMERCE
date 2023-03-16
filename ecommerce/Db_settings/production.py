0b24d3c36873226a64502eaaa6ce77a1591df98f
Tarun Tyagi <tarun.tyagi.cs.2018@mitmeerut.ac.in>
2023-03-16 12:52:54 +0530

    run.sh

--- ecommerce/Db_settings/production.py
+++ ecommerce/Db_settings/production.py
@@ -1,14 +1,14 @@
 from ecommerce.settings import *
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'Ecommerce',
-#         'USER': 'postgres',
-#         'PASSWORD': 'tarunroot',
-#         'HOST': 'database-2.cvuk9hr7ijcp.us-east-2.rds.amazonaws.com',
-#         'PORT': '5432',
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Ecommerce',
+        'USER': 'postgres',
+        'PASSWORD': 'tarunroot',
+        'HOST': 'database-2.cvuk9hr7ijcp.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
        
-#     }
-# }
+    }
+}
